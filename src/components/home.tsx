110c0e06b68136abb516a902d99c8e06e10c03d0
Sergii Mykhailov <sergii.mykhailov@gmail.com>
2019-12-07 19:39:08 +0000

    Added description

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const Home: React.FC = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <h2>Home</h2>
+      <p>Some description should go here</p>
+    </div>
+  );
 };
 
 export default Home;
