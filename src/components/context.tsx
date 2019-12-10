c35fc367632df0554d56d17bb7aad648a2fd9978
Sergii Mykhailov <smykhailov@users.noreply.github.com>
2019-12-10 12:06:11 +0100

    Added few topics to investigate (#1)

---
+++ src/components/context.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Context: React.FC = () => {
+  return (
+    <div>
+      <h2>Context</h2>
+    </div>
+  );
+};
+
+export default Context;
---
+++ src/components/functions.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Functions: React.FC = () => {
+  return (
+    <div>
+      <h2>Functions</h2>
+    </div>
+  );
+};
+
+export default Functions;
