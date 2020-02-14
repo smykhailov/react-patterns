fab4dc8dacdb8efae09504dabf807d320a231760
smykhailov <smykhailov@users.noreply.github.com>
2020-02-14 11:21:38 +0000

    Automated deployment: Fri Feb 14 11:21:38 UTC 2020 848055c14cef464da95b94471de5bd59dfc9f114

---
+++ precache-manifest.290b8270c1866e13183d3c1067c76784.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f613a88aabee56e7f1f038e4fdba6a13",
+    "url": "/react-patterns/index.html"
+  },
+  {
+    "revision": "955dae186f14f8f94bac",
+    "url": "/react-patterns/static/css/2.cbb0499e.chunk.css"
+  },
+  {
+    "revision": "433ef640328ccf9eee68",
+    "url": "/react-patterns/static/css/main.50fbcdfe.chunk.css"
+  },
+  {
+    "revision": "955dae186f14f8f94bac",
+    "url": "/react-patterns/static/js/2.1d53f56e.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/react-patterns/static/js/2.1d53f56e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "433ef640328ccf9eee68",
+    "url": "/react-patterns/static/js/main.5f78d211.chunk.js"
+  },
+  {
+    "revision": "9def1d37b096ae0bd6d0",
+    "url": "/react-patterns/static/js/runtime-main.12dda87e.js"
+  }
+]);
