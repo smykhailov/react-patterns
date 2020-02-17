8b0b681490ef047ea535482379bac1d1fd2c5db7
smykhailov <smykhailov@users.noreply.github.com>
2020-02-17 16:54:26 +0000

    Automated deployment: Mon Feb 17 16:54:26 UTC 2020 afffedfbb266c8e2379eff801cc4b8bfefee0620

--- precache-manifest.83148e1ab0491e49bef56e2b02905299.js
+++ precache-manifest.83148e1ab0491e49bef56e2b02905299.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "311a737a265c086939261cc524e4a614",
+    "revision": "38e1e8defdd95fa88171cd875aafc87f",
     "url": "/react-patterns/index.html"
   },
   {
-    "revision": "7e05f1c7c31c18ff8d3a",
+    "revision": "8091f9055fbfd0b01a1a",
     "url": "/react-patterns/static/css/2.cbb0499e.chunk.css"
   },
   {
-    "revision": "97623cb478718f23453f",
+    "revision": "8a39eafe571faec735e4",
     "url": "/react-patterns/static/css/main.50fbcdfe.chunk.css"
   },
   {
-    "revision": "7e05f1c7c31c18ff8d3a",
-    "url": "/react-patterns/static/js/2.c421610a.chunk.js"
+    "revision": "8091f9055fbfd0b01a1a",
+    "url": "/react-patterns/static/js/2.7d9d2ac4.chunk.js"
   },
   {
     "revision": "e928fe768baa9832b5bc57eae021f30c",
-    "url": "/react-patterns/static/js/2.c421610a.chunk.js.LICENSE"
+    "url": "/react-patterns/static/js/2.7d9d2ac4.chunk.js.LICENSE"
   },
   {
-    "revision": "97623cb478718f23453f",
-    "url": "/react-patterns/static/js/main.80c6f3dc.chunk.js"
+    "revision": "8a39eafe571faec735e4",
+    "url": "/react-patterns/static/js/main.664496b8.chunk.js"
   },
   {
     "revision": "9def1d37b096ae0bd6d0",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-patterns/precache-manifest.298c8f7a22b8203d749646584df0a301.js"
+  "/react-patterns/precache-manifest.83148e1ab0491e49bef56e2b02905299.js"
 );
 
 self.addEventListener('message', (event) => {
